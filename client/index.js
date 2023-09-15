fc62c93ea2c6e2db48d7ada43888f66a0c74e356
Jean-Baptiste Guerin <75306270+Sharkoux@users.noreply.github.com>
2023-09-15 17:19:52 +0200

    feat: Merkle tree gift list OKAY

--- client/index.js
+++ client/index.js
@@ -4,14 +4,26 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const name = 'Norman Block';
+
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // create the merkle tree for the whole nice list
+  const merkleTree = new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n === name);
+  // if the name is in the list, get the proof
+  if (index !== -1) {
+    const proof = merkleTree.getProof(index);
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+    // send the proof and name to the server
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      name, proof
+    });
 
-  console.log({ gift });
+    console.log({ gift });
+  }
+  else {
+    console.log("You are not on the list :(");
+  }
 }
 
 main();
